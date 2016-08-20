e78531a2cd396cb1db83e0f18822775a512c4a4f
DaveG <davegrice@comcast.net>
2016-08-19 20:08:16 -0700

    class1 ex10 script

---
+++ class1/class1_ex10.py
@@ -0,0 +1,17 @@
+from ciscoconfparse import CiscoConfParse
+
+# load exercise config file into variable
+
+input_file = CiscoConfParse("cisco_ipsec.txt")
+
+# find crypto map entries
+
+crypto = input_file.find_objects_wo_child(parentspec=r"^crypto map CRYPTO", childspec=r"transform-set AES")
+
+# print out crypto map entries and their children
+
+for c in crypto:
+  print c.text
+  for child in c.all_children:
+    print child.text
+
