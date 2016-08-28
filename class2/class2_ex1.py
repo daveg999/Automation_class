4ed65bcb63c78497f3dc58b94927fa6b0eb8177f
DaveG <davegrice@comcast.net>
2016-08-27 22:14:13 -0700

    Class2 Exercise 1a&b, ex1-c

---
+++ class2/class2_ex1.py
@@ -0,0 +1,18 @@
+# Python libraries
+# Ex1a
+
+import pysnmp
+import paramiko
+
+# Ex1b
+
+dir(pysnmp)
+ver = pysnmp.__version__
+print "pysnmp version is: %s " % ver 
+
+dir(paramiko)
+ver = paramiko.__version__
+print "paramiko version is: %s " % ver
+
+# Ex1c
+
---
+++ class2/class2_ex1c.py
@@ -0,0 +1,11 @@
+# import my hello function
+import  my_func
+
+# print my_func
+greeting = my_func.print_hello
+print
+print greeting()
+
+# used my_func from /automation_class/class2
+# used my_func from /tmp
+# used my_func from ~/applied_python/lib/python2.7/site-packages/
